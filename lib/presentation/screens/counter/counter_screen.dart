524736c7c304ef3bf8406b4509b71ff9505c8dbb
Luis Parrado <luisprmat@gmail.com>
2025-04-06 22:29:11 -0500

    Counter design - floatingActionButton

--- lib/presentation/screens/counter/counter_screen.dart
+++ lib/presentation/screens/counter/counter_screen.dart
@@ -5,6 +5,20 @@ class CounterScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text('Counter Screen')));
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('10', style: TextStyle(fontSize: 160, fontWeight: FontWeight.w100)),
+            Text('Clicks', style: TextStyle(fontSize: 25)),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.plus_one),
+      ),
+    );
   }
 }
